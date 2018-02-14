7a39700d396a544746ed9ec7b8f049dea55ce225
Sabrina Seibel <seibelsabrina@gmail.com>
2018-02-13 21:53:38 -0500

    Logout button

--- app/src/main/java/io/github/seibelsabrina/shelterme/LoggedInActivity.java
+++ app/src/main/java/io/github/seibelsabrina/shelterme/LoggedInActivity.java
@@ -1,7 +1,9 @@
 package io.github.seibelsabrina.shelterme;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class LoggedInActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class LoggedInActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_logged_in);
+    }
+
+    public void logout(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
