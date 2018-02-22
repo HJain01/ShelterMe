02284f22b9c919aa7e54580e0ac07ff179600d9c
Tanya <Tanya@lawn-143-215-127-4.lawn.gatech.edu>
2018-02-22 12:18:41 -0500

    new hashmap to manually save data instead of a database

--- app/src/main/java/io/github/seibelsabrina/shelterme/RegisterActivity.java
+++ app/src/main/java/io/github/seibelsabrina/shelterme/RegisterActivity.java
@@ -10,10 +10,20 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.EditText;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import io.github.seibelsabrina.shelterme.Model.Admin;
+import io.github.seibelsabrina.shelterme.Model.Model;
+import io.github.seibelsabrina.shelterme.Model.User;
 
 
 public class RegisterActivity extends AppCompatActivity {
@@ -23,7 +33,22 @@ public class RegisterActivity extends AppCompatActivity {
     EditText passwordEnter;
     EditText confirmPW;
 
+
+    private User _user;
+    private Admin _admin;
+    private CheckBox userBox;
+    private CheckBox adminBox;
+    private Map users;
+    private Model instance;
     private FirebaseAuth mAuth;
+    private Map admin;
+
+    final FirebaseDatabase userDatabase = FirebaseDatabase.getInstance();
+    DatabaseReference ref = userDatabase.getReference("users-database-720ef");
+
+    final FirebaseDatabase adminDatabase = FirebaseDatabase.getInstance();
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +58,7 @@ public class RegisterActivity extends AppCompatActivity {
         //firebase app listener - establish connection here
 
         mAuth = FirebaseAuth.getInstance();
+        instance = Model.getInstance();
     }
 
     @Override
@@ -98,4 +124,51 @@ public class RegisterActivity extends AppCompatActivity {
         }
 
     }
+
+//    public void onAddPressed(View view) {
+//        _user.setfirstName(firstName.getText().toString());
+//        _user.setlastName(lastName.getText().toString());
+//        _user.setuserName(regUsername.getText().toString());
+//        _user.setpassWord(passwordEnter.getText().toString());
+//
+//    }
+
+    public void onCheckboxClicked(View view) {
+
+        // Is the view now checked?
+
+        boolean checked = ((CheckBox) view).isChecked();
+        _user = new User();
+        _admin = new Admin();
+        // Check which checkbox was clicked
+        switch(view.getId()) {
+            case R.id.UserBox:
+                if (checked) {
+                    _user.setfirstName(firstName.getText().toString());
+                    _user.setlastName(lastName.getText().toString());
+                    _user.setuserName(regUsername.getText().toString());
+                    _user.setpassWord(passwordEnter.getText().toString());
+                    instance.getUsers().put(_user.getuserName(), _user);
+                    Intent intent = new Intent(this, LoggedInActivity.class);
+                    startActivity(intent);
+                }
+            case R.id.AdminBox:
+                if (checked) {
+                    _admin.setfirstName(firstName.getText().toString());
+                    _admin.setlastName(lastName.getText().toString());
+                    _admin.setuserName(regUsername.getText().toString());
+                    _admin.setpassWord(passwordEnter.getText().toString());
+                    instance.getAdmins().put(_admin.getuserName(), _admin);
+                    Intent intent = new Intent(this, LoggedInActivity.class);
+                    startActivity(intent);
+                }
+        }
+    }
+
+    DatabaseReference usersRef = ref.child("users");
+//    Map<String, String> users = new HashMap<>();
+
+
+    //DatabaseReference adminRef = ref.child("admin");
 }
+
